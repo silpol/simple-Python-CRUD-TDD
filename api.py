506758aab8ceb4e55cca1bd8d9bec58a2c8cca6a
Andriy Tymchenko <silpol@gmail.com>
2022-05-26 21:00:45 +0300

    minimal API call passing single test for GET

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return jsonify({"message":"hello world"})
 
 app.run(debug=True)
 
