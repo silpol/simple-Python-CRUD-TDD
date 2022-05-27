8efdded349aaa88c524cd451e00704ac77daa068
Andriy Tymchenko <silpol@gmail.com>
2022-05-27 10:08:49 +0300

    test plans written in notes at bottom

--- api_test.py
+++ api_test.py
@@ -1,7 +1,8 @@
 import requests
 import pytest, pexpect
 
-class TestAPI():
+
+class TestAPI:
     @pytest.fixture(autouse=True)
     def setup(self):
         self.url = 'http://localhost:80'
@@ -13,20 +14,25 @@ class TestAPI():
         yield
         server.kill(9)
 
-    def test_API_OK(self):
+    def test_api_ok(self):
         r = requests.get("http://localhost:80/api")
         assert r.status_code == 200
-        assert r.json() == {"API":"OK"}
+        assert r.json() == {"API": "OK"}
+
 
 '''
 test plans
-
-1) per each type of object
+1) standard check of CRUD functioning
+2) per each type of object
 -- double insert to check 500 on constraints
 -- double delete to check 500 on constraints
 -- update with empty string in name to check constraints
--- inserts and updates with too long name
-
--- Continent should have delete propagation
--- Country should have delete propagation
+-- inserts and updates with too long name field
+-- duplicate "correct" key name in JSON parameters,
+--- one case with original "new" names (not present in db before)
+--- one case with one already present and another not present
+-- same plus in combination with incorrect ones (few cases possible)
+3) relationships
+-- Continent delete should have delete propagation to countries and then to cities, no dormant "leaves" left
+-- Country should have delete propagation to cities, no dormant "leaves" left
 '''
